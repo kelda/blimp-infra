0ed023ee0fb873c64388513d4f898d464e50c145
Kevin Lin <kevin@kelda.io>
2020-09-15 13:40:29 -0700

    Bump version

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ if [[ $# -lt 1 || $# -gt 2 || "$1" = "--help" ]]; then
 fi
 
 image_registry="gcr.io/kelda-blimp"
-blimp_version="0.13.22"
+blimp_version="0.13.33"
 registry_hostname="$1"
 kubectl_context=""
 if [[ $# -eq 2 ]]; then
